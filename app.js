a2d62f2a0b9c782a22ecff821c35a927ed765375
Hyunsoo <surisuri9632@gmail.com>
2025-02-02 00:22:26 +0900

    app.js sitemap.xml 코드 수정.

--- app.js
+++ app.js
@@ -8,19 +8,16 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Sitemap 포함 라우트 설정
 const routes = require('./routes/index');
 app.use('/', routes);
 
-// 정적 파일 경로 설정
+// 정적 파일 경로 설정 (Sitemap 라우트 뒤에 추가)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser 설정
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// 라우터 설정
-
-
 
 // 서버 시작
 app.listen(PORT, () => {
